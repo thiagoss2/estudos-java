04f92ff653148f57d1b7332ebfe7ff92a2374c2e
Thiago Silva <thiagojklb@gmail.com>
2023-10-09 23:51:48 -0300

    finalizando do exercicio do livro use a cabeça

--- estudos__java/src/estudos__java/BeerSong.java
+++ estudos__java/src/estudos__java/BeerSong.java
@@ -7,6 +7,32 @@ public class BeerSong {
 		int beerNum = 99;
 		String word = "bottles";
 		
+		while (beerNum > 0) {
+			
+			if(beerNum == 1) {
+				word = "bottle" ;  // ou seja so singular
+				
+			}
+			
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+			System.out.println(beerNum + " " + word + " of beer.");
+			System.out.println("Take one down");
+			System.out.println("pass it around");
+			
+			beerNum--;
+           
+			if(beerNum > 0) {
+				System.out.println(beerNum + " " + word + "of beer on the wall");
+				
+			} else {
+				System.out.println("No more bottles of beer on the wall");
+			}
+			
+		
+			
+			
+		} // fim do while
+		
 		
 	}
 }
