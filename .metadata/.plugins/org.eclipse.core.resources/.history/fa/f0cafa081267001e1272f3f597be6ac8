c7aa39a0f9acea131d266a68b38c4a7df0ff604d
Thiago Silva <thiagojklb@gmail.com>
2023-10-09 23:36:10 -0300

    iniciando o projeto

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/fa/f0cafa081267001e1272f3f597be6ac8
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Thiago
+ *
+ */
+module estudos__java {
+}
---
+++ estudos__java/src/estudos__java/BeerSong.java
@@ -0,0 +1,13 @@
+package estudos__java;
+
+public class BeerSong {
+
+	public static void main(String[] args) {
+		
+		 int soma = 2 + 2;
+		 
+		 System.out.println(soma);
+
+	}
+
+}
